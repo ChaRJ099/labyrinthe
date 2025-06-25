0e1b2f16a0831fabe3956eb98a6e2d2b1f7c435b
ramele_c <ramele_c>
2025-06-25 20:17:29 +0200

    Premier essai fonction récursive disc+move

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const BASE_URL = "https://hire-game-maze.pertimm.dev/";
 
 async function startGame() {
+  console.log("C'est parti");
   try {
     const response = await fetch(`${BASE_URL}start-game/`, {
       method: "POST",
@@ -57,6 +58,9 @@ async function discover(discoverURL) {
 }
 
 async function move(moveURL, location) {
+  console.log("Coordonnées envoyées :", location);
+
+  console.log("Appel moveURL :", moveURL);
   try {
     const response = await fetch(moveURL, {
       method: "POST",
@@ -66,10 +70,10 @@ async function move(moveURL, location) {
       body: `position_x=${location.x}&position_y=${location.y}`,
     });
 
+    const data = await response.json();
     if (!response.ok) {
       throw new Error(data.message || `Erreur ${response}`);
     }
-    const data = await response.json();
 
     return data;
   } catch (error) {
@@ -78,35 +82,72 @@ async function move(moveURL, location) {
   }
 }
 
-async function main() {
-  const { discoverURL, moveURL } = await startGame();
-
-  //@TODO: créer boucle automatique pour sortir du labyrinthe
+async function actionDiscMove(discoverURL, moveURL, actualPosition) {
   const discoveredElements = await discover(discoverURL);
-  //@TODO: que faire si plusieurs safePath ?
   //@TODO: que faire si on est bloqué dans un cul de sac ? (enregistrer toutes les cases traversées + chemin qui vient juste d'être emprunté ?=> lastPosition ?)
-  const safePath = discoveredElements.find((element) => element.move === true);
+  //@TODO: ou alors, choisir toujours gauche ou toujours droite comme chemin à suivre sauf si demi-tour ou tout droit ou 1 seul path possible,
+  //       preferer le tout droit plutot que la position opposée de celle par défaut
+  // let safePathList = [];
+  // const safePath = discoveredElements.find((element) => element.move === true);
+  // safePathList.push(safePath);
+  const safePathList = discoveredElements.filter(
+    (element) => element.move === true
+  );
+
   const location = {
-    x: safePath.x,
-    y: safePath.y,
+    x: safePathList[0].x,
+    y: safePathList[0].y,
   };
+  console.log("Position actuelle avant move :", actualPosition);
   const destination = await move(moveURL, location);
+  actualPosition.positionX = destination.position_x;
+  actualPosition.positionY = destination.position_y;
 
-  console.log("discoveredElements", discoveredElements);
-  console.log("safePath", safePath);
-  console.log("location", location);
+  console.log("Position cible envoyée à move :", location);
+  console.log("Position retournée après move :", {
+    x: destination.position_x,
+    y: destination.position_y,
+  });
+
+  // console.log("discoveredElements", discoveredElements);
+  console.log("safePathList", safePathList);
+  // console.log("location", location);
   console.log("destination", destination);
+
+  // console.log("safePath", safePath);
+  if (safePathList.length === 0) {
+    console.log("Aucun chemin possible : fin du jeu ou cul-de-sac.");
+    return;
+  } else if (safePathList.length === 1) {
+    console.log("1 chemin : on relance actionDiscMove");
+    await actionDiscMove(discoverURL, moveURL, actualPosition);
+  } else if (safePathList.length > 1) {
+    // On calcule le chemin à gauche de actualPosition et on move dessus
+    console.log(
+      "Plusieurs chemins : on relance actionDiscMove sur le chemin de gauche"
+    );
+    console.log(safePathList);
+    // actionDiscMove(discoverURL, moveURL);
+  } else {
+    console.log("je sais pas");
+    return;
+  }
+  return;
 }
 
+async function main() {
+  const { discoverURL, moveURL, actualPosition } = await startGame(); // =====> DEBUT DU JEU
+
+  // ========> Si le jeu a démarré
+  if (discoverURL != "") {
+    // ====> on lance actionDiscMove(discoverURL, moveURL);
+    // console.log("discoverURL", discoverURL);
+    await actionDiscMove(discoverURL, moveURL, actualPosition);
+  } else {
+    console.log("oups");
+    console.log(error);
+    return;
+  }
+  return;
+}
 await main();
-
-// implémenter une fonction récursive pour discover/move jusqu'à Win ou Death
-
-function recurs(param) {
-  console.log("param", param);
-  param++;
-  if (param > 10) return;
-  recurs(param);
-}
-
-recurs(0);
