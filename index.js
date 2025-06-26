132944a43c3a046fac3512c8a0a2b664c8dad58d
ramele_c <ramele_c>
2025-06-26 19:12:55 +0200

    Sortie trouvée

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let moveUrl = "";
 // let pathToMove = { positionX: null, positionY: null };
 let availableMoves = [];
 
-let visited = [];
+let visited = {};
 // let actualPosition = { positionX: null, positionY: null };
 
 async function startGame() {
@@ -97,32 +97,44 @@ async function actionDiscMove(discoverURL, moveURL) {
   // 1- On récupère les 4 chemins proposés via discover
   const discoveredElements = await discover(discoverURL);
   // 2- On filtre les 4 chemins proposés pour ne garder que les "move = true" et on les stocke dans un tableau
-  const availableMoves = discoveredElements.filter(
+  let availableMoves = discoveredElements.filter(
     (element) => element.move === true
   );
 
+  console.log("available", availableMoves);
+
   // 3-Savoir combien de fois chaque chemin a été visité
-  availableMoves.forEach((move) => {
-    const key = `${move.x}${move.y}`;
-    const count = visited[key] || 0;
-    console.log(`Case (${key}) visitée ${count} fois`);
-  });
 
-  // 4 - Choisir le move avec le plus petit compteur
+  // if (availableMoves.length === 1) {
 
-  // 5 - move() vers cette case
-  await move(moveURL, positionToSend);
+  if (availableMoves.length > 1) {
+    console.log("visited", visited);
+    availableMoves = availableMoves.filter((move, index) => {
+      console.log("move", `${move.x}${move.y}`);
+      return visited[`${move.x}${move.y}`] === undefined;
+    });
+  }
 
-  // 6 - Incrémenter le compteur pour la nouvelle case
+  // On avance sur le seul ou le premier path dispo
+  const positionSent = {
+    x: availableMoves[0].x,
+    y: availableMoves[0].y,
+  };
+  const dataMove = await move(moveURL, positionSent);
+
+  if (availableMoves[0].value === "stop") {
+    console.log("WIN", dataMove);
+    return;
+  }
+
+  const key = [`${dataMove.position_x}${dataMove.position_y}`];
+  visited[key] = visited[key] ? (visited[key] += 1) : 1;
+  await actionDiscMove(dataMove.url_discover, dataMove.url_move, positionSent);
 }
 
 async function main() {
   const { discoverURL, moveURL, actualPosition } = await startGame();
+  visited[`${actualPosition.x}${actualPosition.y}`] = 1;
   await actionDiscMove(discoverURL, moveURL, actualPosition);
 }
 await main();
-
-// const positionToSend = {
-//   x: availableMoves[0].x,
-//   y: availableMoves[0].y,
-//
